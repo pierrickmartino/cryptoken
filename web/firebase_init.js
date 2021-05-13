40ab2a185d6c4d95a9a64adea14db317cf50b2b3
pierrickmartino <pierrick.martino@gmail.com>
2021-05-13 16:14:36 +0200

    INIT

---
+++ web/firebase_init.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDk5dGCXwaLmLWRh3N7Q0N9uhv73Zrg7z8",
+    authDomain: "cryptoken-64972.firebaseapp.com",
+    projectId: "cryptoken-64972",
+    storageBucket: "cryptoken-64972.appspot.com",
+    messagingSenderId: "895956999617",
+    appId: "1:895956999617:web:b402486aa8563ad905a340"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
