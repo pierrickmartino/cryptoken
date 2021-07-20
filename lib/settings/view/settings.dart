d629e2b05f447a4c1b89b3a91b52f17bc1ebb8ca
pierrickmartino <pierrick.martino@gmail.com>
2021-07-20 17:28:08 +0200

    data refresh is now in setting page

--- lib/settings/view/settings.dart
+++ lib/settings/view/settings.dart
@@ -1,7 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:settings_ui/settings_ui.dart';
+import 'package:synchronized/extension.dart';
 import 'package:web_dashboard/position/controller/position_controller.dart';
+import 'package:web_dashboard/position/model/position_model.dart';
+import 'package:web_dashboard/token/controller/token_controller.dart';
+import 'package:web_dashboard/token/model/price.dart';
+import 'package:web_dashboard/token/model/variation24.dart';
 
 class SettingsUI extends StatefulWidget {
   @override
@@ -29,13 +37,18 @@ class _SettingsScreenState extends State<SettingsUI> {
             tiles: [
               SettingsTile.switchTile(
                 title: 'Show position with zero amount',
-                leading: const Icon(Icons.details),
+                leading: const Icon(Icons.visibility),
                 switchValue: controller.currentZeroPosition,
                 onToggle: (bool value) {
                   //setState(() {
                   controller.setZeroPositionDisplay(value);
                   //});
                 },
+              ),
+              SettingsTile(
+                title: 'Refresh token data',
+                leading: const Icon(Icons.refresh),
+                onPressed: _processRefreshData,
               ),
               const SettingsTile(
                 title: 'Environment',
@@ -120,5 +133,128 @@ class _SettingsScreenState extends State<SettingsUI> {
         ],
       ),
     );
+  }
+
+  /// Retrieve all the available positions for the user.
+  /// Then for each of them, use of the Binance API to get price
+  /// and variation 24h.
+  /// Storage of the position/price and position/var
+  Future<void> _processRefreshData(BuildContext context) async {
+    late List<PositionModel> _positionList;
+    // List<Price> priceList = <Price>[];
+    // List<Variation24> variation24List = <Variation24>[];
+    // double valuation = 0;
+    // double unrealizedGain = 0;
+    final PositionController positionController = PositionController.to;
+    final TokenController tokenController = TokenController.to;
+
+    await synchronized(() async {
+      await positionController.getFirestorePositionList().then((value) {
+        _positionList = value;
+        int _positionPriceCounter = _positionList.length;
+        int _positionVar24Counter = _positionList.length;
+
+        // for each position we need to fetch the last market price
+        for (var i = 0; i < _positionList.length; i++) {
+          final futurePrice = _fetchPrice(_positionList[i].token).then((value) {
+            tokenController
+              ..setTokenPrice(
+                  value.symbol.replaceFirst('USDT', ''), value.price)
+              ..setTokenUpdatedDate(value.symbol.replaceFirst('USDT', ''));
+          }).whenComplete(() {
+            _positionPriceCounter = _positionPriceCounter - 1;
+          });
+
+          final futureVariation24 = _fetchVariation24(_positionList[i].token)
+              .then((value) {
+            tokenController
+              ..setTokenVar24(
+                  value.symbol.replaceFirst('USDT', ''), value.priceChange)
+              ..setTokenVar24Percent(value.symbol.replaceFirst('USDT', ''),
+                  value.priceChangePercent);
+          }).whenComplete(
+                  () => _positionVar24Counter = _positionVar24Counter - 1);
+
+          Future.wait([
+            futurePrice,
+            futureVariation24,
+          ]).whenComplete(() {
+            if (_positionPriceCounter == 0 && _positionVar24Counter == 0) {
+              Get.snackbar<void>(
+                  'Refresh', 'Token market date successfully udpdated !',
+                  snackPosition: SnackPosition.BOTTOM,
+                  duration: const Duration(seconds: 5),
+                  backgroundColor: Get.theme.snackBarTheme.backgroundColor,
+                  colorText: Get.theme.snackBarTheme.actionTextColor);
+            }
+          });
+        }
+      });
+    });
+
+    return;
+  }
+
+  Future<Price> _fetchPrice(String symbol) async {
+    if (symbol == 'INIT') {
+      return Price(price: 0, symbol: '');
+    }
+    if (symbol == 'USDT') {
+      return Price(price: 1, symbol: 'USDT');
+    }
+
+    symbol = '${symbol}USDT';
+
+    final response = await http.get(
+      Uri.parse('https://api3.binance.com/api/v3/ticker/price?symbol=$symbol'),
+    );
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Price.fromJson(
+        jsonDecode(response.body),
+      );
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load price');
+    }
+  }
+
+  Future<Variation24> _fetchVariation24(String symbol) async {
+    if (symbol == 'INIT') {
+      return Variation24(
+        priceChange: 0,
+        symbol: '',
+        priceChangePercent: 0,
+      );
+    }
+    if (symbol == 'USDT') {
+      return Variation24(
+        priceChange: 0,
+        symbol: 'USDT',
+        priceChangePercent: 0,
+      );
+    }
+
+    symbol = '${symbol}USDT';
+
+    final response = await http.get(
+      Uri.parse('https://api3.binance.com/api/v3/ticker/24hr?symbol=$symbol'),
+    );
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+
+      return Variation24.fromJson(
+        jsonDecode(response.body),
+      );
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load price');
+    }
   }
 }
--- lib/wallet/view/my_files.dart
+++ lib/wallet/view/my_files.dart
@@ -1,14 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'package:synchronized/extension.dart';
-import 'package:web_dashboard/position/controller/position_controller.dart';
-import 'package:web_dashboard/position/model/position_model.dart';
-import 'package:web_dashboard/token/controller/token_controller.dart';
-import 'package:web_dashboard/token/model/price.dart';
-import 'package:web_dashboard/token/model/variation24.dart';
+
 import 'package:web_dashboard/wallet/controller/wallet_controller.dart';
 import 'package:web_dashboard/wallet/model/wallet_model.dart';
 
@@ -49,11 +41,6 @@ class MyFiles extends StatelessWidget {
                   ),
                 if (Responsive.isMobile(context))
                   IconButton(
-                    onPressed: _processRefreshData,
-                    icon: const Icon(Icons.refresh),
-                  ),
-                if (Responsive.isMobile(context))
-                  IconButton(
                     onPressed: () {
                       Get.toNamed('/settings');
                     },
@@ -79,19 +66,7 @@ class MyFiles extends StatelessWidget {
                     icon: const Icon(Icons.add),
                     label: const Text('Add New'),
                   ),
-                if (!Responsive.isMobile(context))
-                  ElevatedButton.icon(
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: defaultPadding * 1.5,
-                        vertical: defaultPadding /
-                            (Responsive.isMobile(context) ? 2 : 1),
-                      ),
-                    ),
-                    onPressed: _processRefreshData,
-                    icon: const Icon(Icons.refresh),
-                    label: const Text('Refresh'),
-                  ),
+
                 if (!Responsive.isMobile(context))
                   ElevatedButton.icon(
                     style: TextButton.styleFrom(
@@ -124,66 +99,6 @@ class MyFiles extends StatelessWidget {
         ),
       ],
     );
-  }
-
-  /// Retrieve all the available positions for the user.
-  /// Then for each of them, use of the Binance API to get price
-  /// and variation 24h.
-  /// Storage of the position/price and position/var
-  Future<void> _processRefreshData() async {
-    late List<PositionModel> _positionList;
-    // List<Price> priceList = <Price>[];
-    // List<Variation24> variation24List = <Variation24>[];
-    // double valuation = 0;
-    // double unrealizedGain = 0;
-    final PositionController positionController = PositionController.to;
-    final TokenController tokenController = TokenController.to;
-
-    await synchronized(() async {
-      await positionController.getFirestorePositionList().then((value) {
-        _positionList = value;
-        int _positionPriceCounter = _positionList.length;
-        int _positionVar24Counter = _positionList.length;
-
-        // for each position we need to fetch the last market price
-        for (var i = 0; i < _positionList.length; i++) {
-          final futurePrice = _fetchPrice(_positionList[i].token).then((value) {
-            tokenController
-              ..setTokenPrice(
-                  value.symbol.replaceFirst('USDT', ''), value.price)
-              ..setTokenUpdatedDate(value.symbol.replaceFirst('USDT', ''));
-          }).whenComplete(() {
-            _positionPriceCounter = _positionPriceCounter - 1;
-          });
-
-          final futureVariation24 = _fetchVariation24(_positionList[i].token)
-              .then((value) {
-            tokenController
-              ..setTokenVar24(
-                  value.symbol.replaceFirst('USDT', ''), value.priceChange)
-              ..setTokenVar24Percent(value.symbol.replaceFirst('USDT', ''),
-                  value.priceChangePercent);
-          }).whenComplete(
-                  () => _positionVar24Counter = _positionVar24Counter - 1);
-
-          Future.wait([
-            futurePrice,
-            futureVariation24,
-          ]).whenComplete(() {
-            if (_positionPriceCounter == 0 && _positionVar24Counter == 0) {
-              Get.snackbar<void>(
-                  'Refresh', 'Token market date successfully udpdated !',
-                  snackPosition: SnackPosition.BOTTOM,
-                  duration: const Duration(seconds: 5),
-                  backgroundColor: Get.theme.snackBarTheme.backgroundColor,
-                  colorText: Get.theme.snackBarTheme.actionTextColor);
-            }
-          });
-        }
-      });
-    });
-
-    return;
   }
 }
 
@@ -225,68 +140,5 @@ class FileInfoCardGridView extends StatelessWidget {
         );
       },
     );
-  }
-}
-
-Future<Price> _fetchPrice(String symbol) async {
-  if (symbol == 'INIT') {
-    return Price(price: 0, symbol: '');
-  }
-  if (symbol == 'USDT') {
-    return Price(price: 1, symbol: 'USDT');
-  }
-
-  symbol = '${symbol}USDT';
-
-  final response = await http.get(
-    Uri.parse('https://api3.binance.com/api/v3/ticker/price?symbol=$symbol'),
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Price.fromJson(
-      jsonDecode(response.body),
-    );
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load price');
-  }
-}
-
-Future<Variation24> _fetchVariation24(String symbol) async {
-  if (symbol == 'INIT') {
-    return Variation24(
-      priceChange: 0,
-      symbol: '',
-      priceChangePercent: 0,
-    );
-  }
-  if (symbol == 'USDT') {
-    return Variation24(
-      priceChange: 0,
-      symbol: 'USDT',
-      priceChangePercent: 0,
-    );
-  }
-
-  symbol = '${symbol}USDT';
-
-  final response = await http.get(
-    Uri.parse('https://api3.binance.com/api/v3/ticker/24hr?symbol=$symbol'),
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-
-    return Variation24.fromJson(
-      jsonDecode(response.body),
-    );
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load price');
   }
 }
