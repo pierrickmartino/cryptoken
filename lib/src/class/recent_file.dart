03e60d9bbf1936fceef7643e527615b954de6eeb
pierrickmartino <pierrick.martino@gmail.com>
2021-07-27 20:20:23 +0200

    clean up

--- lib/src/class/recent_file.dart
+++ lib/src/class/recent_file.dart
@@ -1,50 +1,50 @@
-class RecentFile {
-  RecentFile({this.icon, this.title, this.date, this.size});
+// class RecentFile {
+//   RecentFile({this.icon, this.title, this.date, this.size});
 
-  final String? icon, title, date, size;
-}
+//   final String? icon, title, date, size;
+// }
 
-List<RecentFile> demoRecentFiles = [
-  RecentFile(
-    icon: 'icons/xd_file.svg',
-    title: 'XD File',
-    date: '01-03-2021',
-    size: '3.5mb',
-  ),
-  RecentFile(
-    icon: 'icons/Figma_file.svg',
-    title: 'Figma File',
-    date: '27-02-2021',
-    size: '19.0mb',
-  ),
-  RecentFile(
-    icon: 'icons/doc_file.svg',
-    title: 'Documetns',
-    date: '23-02-2021',
-    size: '32.5mb',
-  ),
-  RecentFile(
-    icon: 'icons/sound_file.svg',
-    title: 'Sound File',
-    date: '21-02-2021',
-    size: '3.5mb',
-  ),
-  RecentFile(
-    icon: 'icons/media_file.svg',
-    title: 'Media File',
-    date: '23-02-2021',
-    size: '2.5gb',
-  ),
-  RecentFile(
-    icon: 'icons/pdf_file.svg',
-    title: 'Sals PDF',
-    date: '25-02-2021',
-    size: '3.5mb',
-  ),
-  RecentFile(
-    icon: 'icons/excle_file.svg',
-    title: 'Excel File',
-    date: '25-02-2021',
-    size: '34.5mb',
-  ),
-];
+// List<RecentFile> demoRecentFiles = [
+//   RecentFile(
+//     icon: 'icons/xd_file.svg',
+//     title: 'XD File',
+//     date: '01-03-2021',
+//     size: '3.5mb',
+//   ),
+//   RecentFile(
+//     icon: 'icons/Figma_file.svg',
+//     title: 'Figma File',
+//     date: '27-02-2021',
+//     size: '19.0mb',
+//   ),
+//   RecentFile(
+//     icon: 'icons/doc_file.svg',
+//     title: 'Documetns',
+//     date: '23-02-2021',
+//     size: '32.5mb',
+//   ),
+//   RecentFile(
+//     icon: 'icons/sound_file.svg',
+//     title: 'Sound File',
+//     date: '21-02-2021',
+//     size: '3.5mb',
+//   ),
+//   RecentFile(
+//     icon: 'icons/media_file.svg',
+//     title: 'Media File',
+//     date: '23-02-2021',
+//     size: '2.5gb',
+//   ),
+//   RecentFile(
+//     icon: 'icons/pdf_file.svg',
+//     title: 'Sals PDF',
+//     date: '25-02-2021',
+//     size: '3.5mb',
+//   ),
+//   RecentFile(
+//     icon: 'icons/excle_file.svg',
+//     title: 'Excel File',
+//     date: '25-02-2021',
+//     size: '34.5mb',
+//   ),
+// ];
